adb3677e42a7ff947811d2e9343f48016f2059d9
chance <ch4nc3l0@gmail.com>
2019-07-28 11:29:00 -0400

    Feat: add confirmation messages

--- javascript.js
+++ javascript.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
-    let todos = ["Example Todo"];
+    let todos = [];
     let input = prompt("type something")
 
 
     while (input !== "exit"){
         // List out todos
         if (input === "list"){
-            console.log(todos);
+            // format todos for easy reading
+            console.log("********************");
+            console.log("Here are your todos");
+            todos.forEach(function(list, i){
+                console.log(i, list);
+            });
+            console.log("********************");
         }
         // Add to todos
         else if (input === "new"){
+            console.log("Ok enter a new todo entry")
             let newtodo = prompt("enter new todo")
             todos.push(newtodo)
+            console.log(newtodo + " added to todo list")
         }
         // Loop Question
         input = prompt("type something")
     }
+    // Exit the application
+    console.log("Okay you quit the application")
 
 });
 
