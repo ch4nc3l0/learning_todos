6db0f3b659f8ed3f350f7111b9904e85ca5e289d
chance <ch4nc3l0@gmail.com>
2019-07-28 10:09:26 -0400

    Feat: able to list out todos in console

---
+++ javascript.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+    let todos = ["Example Todo"];
+    let input = prompt("type something")
+
+
+    while (input !== "exit"){
+        // List out todos
+        if (input === "list"){
+            console.log(todos);
+        }
+        // Add to todos
+
+
+        // Loop Question
+
+    }
+
+});
+
+
