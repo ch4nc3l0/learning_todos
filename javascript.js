e081d570a5087a8e3fd50b316b78cc55faf06a2b
chance <ch4nc3l0@gmail.com>
2019-07-28 11:49:24 -0400

    Feat: add delete todos

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(event){
             console.log("********************");
             console.log("Here are your todos");
             todos.forEach(function(list, i){
-                console.log(i, list);
+                console.log(i + ": " + list);
             });
             console.log("********************");
         }
@@ -21,6 +21,15 @@ document.addEventListener("DOMContentLoaded", function(event){
             let newtodo = prompt("enter new todo")
             todos.push(newtodo)
             console.log(newtodo + " added to todo list")
+        }
+        // Delete todo
+        else if (input === "delete"){
+            console.log("Please enter index to delete todo")
+            let deleteTodo = prompt("Enter index of todo to delete")
+            // Add check to make sure is a valid number
+            console.log("Deleted " + todos[deleteTodo] );
+            todos.splice(deleteTodo,1)
+            
         }
         // Loop Question
         input = prompt("type something")
